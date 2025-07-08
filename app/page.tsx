2730cd1199164388aae53e3fcd27b415d38b80a4
Michael <michael@jkt1.ebdesk.com>
2025-07-08 15:54:05 +0700

    update scss

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,15 @@ import { Button } from "react-bootstrap";
 
 export default function Home() {
   return (
-    <div
-      className="tw-p-4 tw:space-y-3"
-      style={{ backgroundColor: "var(--black-300)" }}
-    >
-      <h2 className="tw:text-xl tw:font-semibold" style={{ color: "var(--black)" }}>Tailwind Heading</h2>
+    <div className="tw-p-4 tw:space-y-3">
+      <h2 className="tw:text-xl tw:font-semibold">Tailwind Heading</h2>
       <p className="lead">Bootstrap Lead</p>
-      <p className="tw:text-sm fw-800">Tailwind Small Text</p>
+      <p
+        className="tw:text-sm fw-800"
+        style={{ backgroundColor: "var(--black)", color: "var(--white)" }}
+      >
+        Tailwind Small Text
+      </p>
       <Button>Bootstrap Button</Button>
     </div>
   );
