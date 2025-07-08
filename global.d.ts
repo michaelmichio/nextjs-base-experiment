dbce5f65a7643451f1aa83645b3522a9582ad328
Michael Michio <87609082+michaelmichio@users.noreply.github.com>
2025-07-08 13:45:20 +0700

    Revert "Development"

--- global.d.ts
+++
@@ -1,9 +0,0 @@
-import "react";
-
-declare module "react" {
-  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
-    theme?: string;
-    "theme-mode"?: string;
-    "theme-fontsize"?: string;
-  }
-}
